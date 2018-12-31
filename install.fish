d57aa2700f0a6081abab6b9271dd910430d4fa11
Sobaya <sobaya007@gmail.com>
2018-12-31 17:44:59 +0900

    po

--- install.fish
+++ install.fish
@@ -12,3 +12,5 @@ mkdir ~/.vimfiles
 mkdir ~/.vimfiles/backup
 mkdir ~/.vimfiles/undo
 mkdir ~/git
+
+cd; and cd git; and git clone git@github.com:sourcegraph/javascript-typescript-langserver.git; and cd javascript-typescript-languageserver; and npm install; and npm run build;
